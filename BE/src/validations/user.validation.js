90699305fcbb621fcc70fa02782cf1994d79f6d6
sanghhwork <sanghh.work@gmail.com>
2026-01-20 22:17:19 +0700

    chore: update user validation

--- BE/src/validations/user.validation.js
+++ BE/src/validations/user.validation.js
@@ -33,8 +33,9 @@ const updateUser = {
   body: Joi.object()
     .keys({
       email: Joi.string().email(),
-      password: Joi.string().custom(password),
+      // password: Joi.string().custom(password),
       name: Joi.string(),
+      role: Joi.string().required().valid('user', 'admin'),
     })
     .min(1),
 };
