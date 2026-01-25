b5e6ac0291e21f74cf24a4a427b5236f091a7979
sanghhwork <sanghh.work@gmail.com>
2026-01-25 13:55:09 +0700

    chore: update send mail

--- BE/src/config/config.js
+++ BE/src/config/config.js
@@ -23,6 +23,7 @@ const envVarsSchema = Joi.object()
     SMTP_USERNAME: Joi.string().description('username for email server'),
     SMTP_PASSWORD: Joi.string().description('password for email server'),
     EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+    APP_BASE_URL: Joi.string().uri().description('Base URL of the front-end application used in email links'),
   })
   .unknown();
 
@@ -61,4 +62,5 @@ module.exports = {
     },
     from: envVars.EMAIL_FROM,
   },
+  appBaseUrl: envVars.APP_BASE_URL || 'http://link-to-app',
 };
