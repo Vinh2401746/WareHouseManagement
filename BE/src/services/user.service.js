616c4f9936bca48cccc8c41d40965097b2589162
sanghhwork <sanghh.work@gmail.com>
2026-03-20 23:41:47 +0700

    chore: return branch when login

--- BE/src/services/user.service.js
+++ BE/src/services/user.service.js
@@ -58,7 +58,7 @@ const getUserById = async (id) => {
  * @returns {Promise<User>}
  */
 const getUserByEmail = async (email) => {
-  return User.findOne({ email });
+  return User.findOne({ email }).populate('branch');
 };
 
 /**
